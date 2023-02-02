5c86536602fbe781017c014db5d816f1011426fe
Emma-Nangida <emmanangida@gmail.com>
2023-02-02 22:08:26 +0300

    Initial commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,16 @@
+fun main() {
+
+    var name="Bryton"
+    var age="12"
+    var phonenumber="0788991213"
+    var weightinkgs="23"
+    var Citizenship= true
+
+    println(name)
+    println(age)
+    println(phonenumber)
+    println(weightinkgs)
+    println(Citizenship)
+
+
+}
